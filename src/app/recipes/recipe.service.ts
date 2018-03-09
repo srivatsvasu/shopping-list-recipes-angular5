d29ed771d5ad8bb38f33840da0a7e24f04a5a94d
srivatsvasu <Srivatsan.vasudevan@bnsf.com>
2018-03-08 22:56:41 -0600

    Angular 5 Recipe - shopping List app

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe('Tasty Schinitzel', 'Super tasty Schinitzel - Just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/220px-Wiener-Schnitzel02.jpg', [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger', 'What else do you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/250px-Hamburger_%28black_bg%29.jpg', [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 40)
+      ])
+
+  ];
+
+  constructor(private slService: ShoppingListService) { }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe( index: number) {
+    return this.recipes[index];
+  }
+
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+}
