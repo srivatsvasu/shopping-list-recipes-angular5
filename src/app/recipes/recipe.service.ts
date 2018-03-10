3470c26d3f488c9f1983c7a7e35c1d6406bb4be3
srivatsvasu <Srivatsan.vasudevan@bnsf.com>
2018-03-09 22:22:28 -0600

    add new changes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe('Tasty Schinitzel', 'Super tasty Schinitzel - Just awesome!',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/220px-Wiener-Schnitzel02.jpg', [
@@ -27,7 +26,7 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
-  getRecipe( index: number) {
+  getRecipe(index: number) {
     return this.recipes[index];
   }
 
